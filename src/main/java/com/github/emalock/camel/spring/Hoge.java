6271591838a3edd2c0cb976d100f9641ca9462dc
Shinobu Aoki <emalock3@gmail.com>
2014-02-14 11:36:04 +0900

    コンストラクタでの注入するパターンを追加

--- src/main/java/com/github/emalock/camel/spring/Hoge.java
+++ src/main/java/com/github/emalock/camel/spring/Hoge.java
@@ -1,11 +1,20 @@
 package com.github.emalock.camel.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Hoge {
 
+	private final HogeHoge hogehoge;
+	
+	@Autowired
+	public Hoge(HogeHoge hogehoge) {
+		this.hogehoge = hogehoge;
+	}
+	
 	public void hoge(String name) {
 		System.out.println("hoge " + name);
+		hogehoge.hogehoge();
 	}
 }
---
+++ src/main/java/com/github/emalock/camel/spring/HogeHoge.java
@@ -0,0 +1,12 @@
+package com.github.emalock.camel.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HogeHoge {
+
+	public void hogehoge() {
+		System.out.println("hogehoge");
+	}
+	
+}
